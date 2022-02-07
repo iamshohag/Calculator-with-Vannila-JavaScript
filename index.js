34b4d77c476a4e4c690394cbcb8ea7c7ea9168ab
iamshohag <withoutyou.sk@gmail.com>
2022-02-07 09:54:43 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+var power = -1;
+// 1 is power on -1 is power off. start from power off.
+function off() {
+    power = -power;
+    if (power == -1) {
+        document.getElementById("display").value = "";
+    } else {
+        welcome();
+    }
+
+};
+
+function Erase() {
+    if (power == -1) {
+        document.getElementById("display").value = "";
+    } else {
+        document.getElementById("display").value = "";
+        document.getElementById("display").focus();
+    }
+};
+
+function welcome() {
+    document.getElementById("display").value = "Codded By Shohag Khan";
+    setTimeout(welcome_end, 2000);
+};
+
+function welcome_end() {
+    document.getElementById("display").value = "";
+    document.getElementById("display").focus();
+};
+
+
+function get(value) {
+    if (power == 1) {
+        document.getElementById("display").value += value;
+    } else {
+        document.getElementById("display").value = "";
+    }
+
+
+};
+
+function Result() {
+    if (power == 1) {
+        var result = 0;
+        result = document.getElementById("display").value;
+        document.getElementById("display").value = "";
+        document.getElementById("display").value = eval(result);
+    } else {
+        document.getElementById("display").value = "";
+    }
+};
